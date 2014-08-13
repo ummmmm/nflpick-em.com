25f0bf111ea0349f5f0b17b1ed834651f6e06a18
ummmmm <ummmmm@users.noreply.github.com>
2014-08-13 12:07:17 -0700

    Changed the include file path to the proper /home4/* directory

--- json.php
+++ json.php
@@ -3,7 +3,7 @@ header( 'Content-type: application/json' );
 
 $document_root = $_SERVER[ 'DOCUMENT_ROOT' ];
 
-require_once( '/home1/dcarver/data/db.php' );
+require_once( '/home4/dcarver/data/db.php' );
 require_once( $document_root . '/includes/classes/functions.php' );
 require_once( $document_root . '/includes/classes/database.php' );
 require_once( $document_root . '/includes/classes/validation.php' );
