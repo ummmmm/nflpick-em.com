56e5c66cece3bcb2798c26677c89fa3d555ac036
ummmmm <ummmmm@users.noreply.github.com>
2014-08-15 15:27:03 -0700

    Moved the Sessions class out of functions.php
    The Sessions class has been moved out of functions.php and into
    Sessions.php. In addition, Sessions is slowly being converted over
    into using the database class defined in the constructor instead of
    passing it around in all of the static functions

---
+++ includes/classes/Sessions.php
@@ -0,0 +1,126 @@
+<?php
+include_once( 'database.php' );
+
+class Sessions
+{
+	private $_db;
+	private $_error;
+
+	public function __construct()
+	{
+		$this->_db = new Database();
+	}
+
+	public function __destruct()
+	{
+
+	}
+
+	public function Generate( $user_id )
+	{
+		$cookie_id	= sha1( session_id() );
+		$token		= sha1( uniqid( rand(), TRUE ) );
+		$session	= array( 'token' => $token, 'cookie_id' => $cookie_id, 'user_id' => $user_id );
+
+		setcookie( 'session', $cookie_id, time() + 60 * 60 * 24 * 30, '/' );
+		
+		return $this->_Insert( $session );
+	}
+
+	private function _Insert( $session )
+	{
+		return $this->_db->query( 'INSERT INTO Sessions ( token, cookie_id, user_id ) VALUES ( ?, ?, ? )', $session[ 'token' ], $session[ 'cookie_id' ], $session[ 'user_id' ] );
+	}
+
+	public function Load( $cookie_id, &$session )
+	{
+		return $this->_db->single( 'SELECT * FROM sessions WHERE cookieid = ?', $session, $cookie_id );
+	}
+
+	/*public function Delete( $cookie_id )
+	{
+		return $this->_db->query( 'DELETE FROM Sessions WHERE cookie_id = ?', $cookie_id );
+	}
+
+	public function Delete_UserID( $user_id )
+	{
+		return $this->_db->query( 'DELETE FROM Sessions WHERE user_id = ?', $user_id );
+	}*/
+
+	private function _Error( $message )
+	{
+		$this->_error = $message;
+
+		return false;
+	}
+
+	public function Get_Error()
+	{
+		return $this->_error;
+	}
+
+
+
+	public static function Insert( &$db, $session )
+	{
+		$date = Functions::Timestamp();
+
+		return $db->query( 'INSERT INTO sessions ( token, cookieid, userid, date ) VALUES( ?, ?, ?, ? )', $session[ 'token' ], $session[ 'cookieid' ], $session[ 'userid' ], $date );
+	}
+	
+	public static function Delete( &$db, $token )
+	{
+		return $db->query( 'DELETE FROM sessions WHERE token = ?', $token );
+	}
+	
+	public static function Delete_User( &$db, $user_id )
+	{
+		return $db->query( 'DELETE FROM sessions WHERE userid = ?', $user_id );
+	}
+	
+	public static function Delete_Cookie( &$db, $cookie_id )
+	{
+		return $db->query( 'DELETE FROM sessions WHERE cookieid = ?', $cookie_id );
+	}
+	
+	public static function Update( &$db, $session )
+	{
+		return $db->query( 'UPDATE sessions SET token = ?, userid = ?, date = ? WHERE cookieid = ?', $session[ 'token' ], $session[ 'userid' ], $session[ 'date' ], $session[ 'cookieid' ] );
+	}
+
+	public static function Update_Cookie_LastActive( &$db, $cookieid )
+	{
+		$date = Functions::Timestamp();
+
+		return $db->query( 'UPDATE sessions SET last_active = ? WHERE cookieid = ?', $date, $cookieid );
+	}
+	
+	public static function Validate( &$db, $userid, $token )
+	{
+		$cookieid = Functions::Cookie( 'session' );
+		
+		$count = $db->single( 'SELECT * FROM sessions WHERE token = ? AND cookieid = ? AND userid = ?', $null, $token, $cookieid, $userid );
+		
+		return $count ? true : false;
+	}
+
+	public static function Validate_Admin( &$db, $userid, $token )
+	{
+		$cookieid = Functions::Cookie( 'session' );
+		
+		$count = $db->single( 'SELECT
+								u.id
+							   FROM
+								sessions s,
+								users u
+							   WHERE
+								s.token 	= ? 	AND
+								s.cookieid 	= ? 	AND
+								s.userid 	= ? 	AND
+								s.userid 	= u.id 	AND
+								u.admin 	= 1', $null, $token, $cookieid, $userid );
+
+		return $count ? true : false;
+	}
+}
+?>
