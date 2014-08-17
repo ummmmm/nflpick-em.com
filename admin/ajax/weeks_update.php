788267b5ae2839289b512b43f1744c3e93069f46
ummmmm <carve006@csusm.edu>
2014-08-17 14:50:09 -0700

    Fixed passed by reference bug
    Fixed an issue where a variable that was passed by
    reference into a function was then again dereferenced

--- admin/ajax/weeks_update.php
+++ admin/ajax/weeks_update.php
@@ -8,26 +8,26 @@ function Module_JSON( &$db, &$user )
 	{
 		return JSON_Response_Error( 'NFL-WEEKS_UPDATE-0', 'You do not have a valid token to complete this action.' );
 	}
-	
-	$count = Weeks::Load( &$db, $week_id, $week );
-	
+
+	$count = Weeks::Load( $db, $week_id, $week );
+
 	if ( $count === false )
 	{
 		return JSON_Response_Global_Error();
 	}
-	
+
 	if ( $count === 0 )
 	{
 		return JSON_Response_Error( '#Error', 'Failed to load week' );
 	}
-	
+
 	$week[ 'locked' ] = ( $week[ 'locked' ] === 1 ) ? 0 : 1;
-	
+
 	if ( !Weeks::Update( $db, $week ) )
 	{
 		return JSON_Response_Global_Error();
 	}
-	
+
 	return JSON_Response_Success();
 }
 ?>
