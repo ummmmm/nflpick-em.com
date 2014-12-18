d79bb545cd12f6333a5e1995f5970ab1397e0a32
ummmmm <ummmmm@users.noreply.github.com>
2014-12-18 12:17:11 -0800

    Moved all functionality into the Games DB class
    The create games functionality has been moved into the Games DB class.
    The Games_Create method is now triggered on site install, but may also
    be triggered from this module.

--- html/admin/modules/games_create.php
+++ html/admin/modules/games_create.php
@@ -5,68 +5,25 @@ function Module_Head()
 	return true;
 }
 
-function Module_Content( &$db, &$user )
+function Module_Content( &$db )
 {
-	print '<h1>Games Insert</h1>';
+	$db_games 	= new Games( $db );
+	$db_picks	= new Picks( $db );
 
-	$db_games	= new Games( $db ):
-	$db_teams	= new Teams( $db );
-	$db_weeks	= new Weeks( $db );
-	$count 		= 0;
-	$skip		= 0;
-	$url 		= 'http://football.myfantasyleague.com/2014/export?TYPE=nflSchedule&W=';
+	print '<h1>Games Create</h1>';
 
-	for ( $i = 1; $i <= 17; $i++ )
+	if ( $db_games->List_Load( $null ) || $db_picks->List_Load( $null ) )
 	{
-		$xml = simplexml_load_file( sprintf( '%s%d', $url, $i ) );
-
-		foreach ( $xml->matchup as $matchup )
-		{
-			$kickoff 	= ( int ) 		$matchup[ 'kickoff' ];
-			$team1 		= ( string ) 	$matchup->team[ 0 ][ 'id' ];
-			$team2 		= ( string ) 	$matchup->team[ 1 ][ 'id' ];
-
-			if ( !$db_weeks->Load( $i, $null ) )
-			{
-				return Functions::Error( '#Error#', "Failed to load week {$i}" );
-			}
-
-			if ( !$db_teams->Load_Abbr( $team1, $loaded_team1 ) || !$db_teams->Load_Abbr( $team2, $loaded_team2 ) )
-			{
-				return Functions::Error( '#Error#', "Failed to load either {$team1} or {$team2}" );
-			}
-
-			if ( ( int ) $matchup->team[ 0 ][ 'isHome' ] )
-			{
-				$away = $loaded_team2[ 'id' ];
-				$home = $loaded_team1[ 'id' ];
-			}
-			else
-			{
-				$away = $loaded_team1[ 'id' ];
-				$home = $loaded_team2[ 'id' ];	
-			}
-
-			if ( $db_games->Exists_Week_Teams( $i, $home, $away, $null ) )
-			{
-				$skip++;
-				continue;
-			}
-
-			$gamedate 	= new DateTime();
-			$gamedate->setTimestamp( $kickoff );
-			$game 		= array( 'away' => $away, 'home' => $home, 'date' => $gamedate->format( 'Y-m-d H:i:s' ), 'week' => $i );
-
-			if ( !$db_games->Insert( $game ) )
-			{
-				return Functions::Error();
-			}
-
-			$count++;
-		}
+		print '<p>The games and picks tables must be empty</p>';
 	}
-
-	printf( 'Inserted %d games, skipped %d', $count, $skip );
+	else if ( !$db_games->Create_Games() )
+	{
+		printf( '<p>Failed to create games: %s</p>', htmlentities( $db_games->Get_Error() ) );
+	}
+	else
+	{
+		print '<p>Games created</p>';
+	}
 
 	return true;
 }
