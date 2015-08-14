0504c6a59361247339d2a8d23b1ef010128a0100
ummmmm <ummmmm@users.noreply.github.com>
2015-08-14 16:56:44 -0700

    Converted from JSON module to JSON class

---
+++ html/includes/JSON/LoadPolls.php
@@ -0,0 +1,94 @@
+<?php
+
+class JSON_LoadPolls implements iJSON
+{
+	public function __construct( Database &$db, Authentication &$auth, JSON &$json )
+	{
+		$this->_db		= $db;
+		$this->_auth	= $auth;
+		$this->_json	= $json;
+	}
+
+	public function requirements()
+	{
+		return array( 'user' => true );
+	}
+
+	public function execute()
+	{
+		$db_polls			= new Polls( $this->_db );
+		$db_poll_votes		= new Poll_Votes( $this->_db );
+		$db_poll_answers	= new Poll_Answers( $this->_db );
+		$nav_poll 			= Functions::Post( 'nav' );
+		
+		if ( $nav_poll === '1' )
+		{
+			$count = $db_polls->Latest( $loaded_polls );
+		} else {
+			$count = $db_polls->List_Load( $loaded_polls );
+		}
+		
+		if ( $count === false )
+		{
+			return $this->_json->DB_Error();
+		}
+		
+		foreach( $loaded_polls as &$poll )
+		{
+			$count = $db_poll_answers->List_Load_Poll( $poll[ 'id' ], $answers );
+			
+			if ( $count === false )
+			{
+				return $this->_json->DB_Error();
+			}
+			
+			$vote_count = $db_poll_votes->Total_Poll( $poll[ 'id' ] );
+			
+			if ( $vote_count === false )
+			{
+				return $this->_json->DB_Error();
+			}
+			
+			$poll[ 'total_votes' ] = $vote_count;
+			
+			foreach( $answers as &$answer )
+			{
+				$answer_count = $db_poll_votes->Total_Answer( $answer[ 'id' ] );
+				
+				if ( $answer_count === false )
+				{
+					return $this->_json->DB_Error();
+				}
+				
+				$answer[ 'total_votes' ] = $answer_count;
+			}
+			
+			$poll[ 'answers' ] = $answers;
+			
+			if ( !$this->_auth->authenticated )
+			{
+				$poll[ 'voted' ] = true;
+			}
+			else
+			{
+				$count = $this->_Vote_Casted( $this->_auth->userID, $poll[ 'id' ] );
+				
+				if ( $count === false )
+				{
+					return $this->_json->DB_Error();
+				}
+				
+				$poll[ 'voted' ] = ( $count !== 0 ) ? true : false;
+			}
+		}
+
+		return $this->_json->setData( $loaded_polls );
+	}
+
+	// Helper functions
+
+	private function _Vote_Casted( $user_id, $poll_id )
+	{
+		return $this->_db->single( 'SELECT * FROM poll_votes WHERE user_id = ? AND poll_id = ?', $null, $user_id, $poll_id );
+	}
+}
