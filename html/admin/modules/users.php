793cc595b5aedf94dc51ea30b191689e2a7770f7
ummmmm <ummmmm@users.noreply.github.com>
2015-08-14 16:19:45 -0700

    Updated the sort value to LoadUsers to match new convention

--- html/admin/modules/users.php
+++ html/admin/modules/users.php
@@ -1,7 +1,7 @@
 <?php
 function Module_Head( &$db, &$user, &$settings, &$jquery )
 {
-	$jquery = '$.fn.sort( \'users_load\', \'name\', $.fn.sort_user_callback );';
+	$jquery = '$.fn.sort( \'LoadUsers\', \'name\', $.fn.sort_user_callback );';
 	
 	return true;
 }
@@ -22,13 +22,13 @@ function Module_Content( &$db, &$user )
 	
 	print '<h1>Users</h1>';
 	print '<div class="sortby">Sort By: ';
-	print '<a href="javascript:;" id="name" direction="asc" onclick="$.fn.sort( \'users_load\', \'name\', $.fn.sort_user_callback );">Name</a>';
-	print '<a href="javascript:;" id="current_place" direction="asc" onclick="$.fn.sort( \'users_load\', \'current_place\', $.fn.sort_user_callback );">Current Place</a>';
-	print '<a href="javascript:;" id="last_on" direction="desc" onclick="$.fn.sort( \'users_load\', \'last_on\', $.fn.sort_user_callback );">Last Active</a>';
-	print '<a href="javascript:;" id="paid" direction="asc" onclick="$.fn.sort( \'users_load\', \'paid\', $.fn.sort_user_callback );">Paid</a>';
-	print '<a href="javascript:;" id="failed_logins" direction="asc" onclick="$.fn.sort( \'users_load\', \'failed_logins\', $.fn.sort_user_callback );">Failed Logins</a>';
-	print '<a href="javascript:;" id="active_sessions" direction="desc" onclick="$.fn.sort( \'users_load\', \'active_sessions\', $.fn.sort_user_callback );">Active Sessions</a>';
-	print '<a href="javascript:;" id="remaining" direction="asc" onclick="$.fn.sort( \'users_load\', \'remaining\', $.fn.sort_user_callback );">Remaining Picks</a>';
+	print '<a href="javascript:;" id="name" direction="asc" onclick="$.fn.sort( \'LoadUsers\', \'name\', $.fn.sort_user_callback );">Name</a>';
+	print '<a href="javascript:;" id="current_place" direction="asc" onclick="$.fn.sort( \'LoadUsers\', \'current_place\', $.fn.sort_user_callback );">Current Place</a>';
+	print '<a href="javascript:;" id="last_on" direction="desc" onclick="$.fn.sort( \'LoadUsers\', \'last_on\', $.fn.sort_user_callback );">Last Active</a>';
+	print '<a href="javascript:;" id="paid" direction="asc" onclick="$.fn.sort( \'LoadUsers\', \'paid\', $.fn.sort_user_callback );">Paid</a>';
+	print '<a href="javascript:;" id="failed_logins" direction="asc" onclick="$.fn.sort( \'LoadUsers\', \'failed_logins\', $.fn.sort_user_callback );">Failed Logins</a>';
+	print '<a href="javascript:;" id="active_sessions" direction="desc" onclick="$.fn.sort( \'LoadUsers\', \'active_sessions\', $.fn.sort_user_callback );">Active Sessions</a>';
+	print '<a href="javascript:;" id="remaining" direction="asc" onclick="$.fn.sort( \'LoadUsers\', \'remaining\', $.fn.sort_user_callback );">Remaining Picks</a>';
 	print '</div>';
 	print '<div id="users_loading">Loading...</div>';
 	
