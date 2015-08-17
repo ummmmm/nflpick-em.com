294b115f57e9c69955cd7fb4327cbef1b286f20c
ummmmm <ummmmm@users.noreply.github.com>
2015-08-17 14:35:26 -0700

    Prefixed all private class members with _

--- html/includes/classes/Mail.php
+++ html/includes/classes/Mail.php
@@ -2,64 +2,70 @@
 
 class Mail
 {
-	private $to;
-	private $subject;
-	private $message;
-	private $headers;
-	private $from 		= 'NFL Pick-Em <contact@nflpick-em.com>';
-	private $replyto 	= 'NFL Pick-Em <contact@nflpick-em.com>';
-	
+	private $_to;
+	private $_subject;
+	private $_message;
+	private $_headers;
+	private $_from;
+	private $_replyto;
+
 	public function __construct( $to = null, $subject = null, $message = null, $from = null, $replyto = null, $headers = null )
 	{
-		if ( !is_null( $to ) )		$this->to 		= $to;
-		if ( !is_null( $subject ) )	$this->subject 	= $subject;		
-		if ( !is_null( $message ) )	$this->message 	= $message;
-		if ( !is_null( $from ) )	$this->from 	= $from;
-		if ( !is_null( $replyto ) )	$this->replyto 	= $replyto;
-		if ( !is_null( $headers ) )	$this->headers 	= $headers;
+		$this->_from 	= "NFL Pick-Em <contact@nflpick-em.com>";
+		$this->_replyto	= "NFL Pick-Em <contact@nflpick-em.com>";
+
+		if ( !is_null( $to ) )		$this->_to 		= $to;
+		if ( !is_null( $subject ) )	$this->_subject = $subject;
+		if ( !is_null( $message ) )	$this->_message = $message;
+		if ( !is_null( $from ) )	$this->_from 	= $from;
+		if ( !is_null( $replyto ) )	$this->_replyto = $replyto;
+		if ( !is_null( $headers ) )	$this->_headers = $headers;
 	}
-	
+
 	public function to( $to )
 	{
-		$this->to = $to;
+		$this->_to = $to;
 	}
-	
+
 	public function subject( $subject )
 	{
-		$this->subject = $subject;
+		$this->_subject = $subject;
 	}
-	
+
 	public function message( $message )
 	{
-		$this->message = $message;
+		$this->_message = $message;
 	}
-	
+
 	public function from( $from )
 	{
-		$this->from = $from;
+		$this->_from = $from;
 	}
-	
+
 	public function replyto( $replyto )
 	{
-		$this->replyto = $replyto;
+		$this->_replyto = $replyto;
 	}
-	
+
 	public function headers( $headers )
 	{
-		$this->headers = $headers;
+		$this->_headers = $headers;
 	}
-	
+
 	public function send()
 	{
-		$headers = 'MIME-Version: 1.0' . "\r\n";
-		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-		$headers .= 'From: ' . $this->from . "\r\n";
-		$headers .= 'Reply-To: ' . $this->replyto . "\r\n";
-		$headers .= $this->headers;
-		
-		$message = preg_replace( "/\n/", '<br />', $this->message );
-		
-		if ( !mail( $this->to, $this->subject, $message, $headers ) )
+		$headers = array();
+
+		array_push( $headers, "MIME-Version: 1.0" );
+		array_push( $headers, "Content-type: text/html; charset=utf-8" );
+		array_push( $headers, sprintf( "From: %s", $this->_from ) );
+		array_push( $headers, sprintf( "Reply-To: %s", $this->_replyto ) );
+		array_push( $headers, $this->_headers );
+
+		$headers = implode( "\r\n", $headers );
+		$message = preg_replace( "/\n/", '<br />', $this->_message );
+
+		if ( !mail( $this->_to, $this->_subject, $message, $headers ) )
 		{
 			return Functions::Error( '#Error#', 'Failed to send mail' );
 		}
