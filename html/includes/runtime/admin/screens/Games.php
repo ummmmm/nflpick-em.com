ae269a6bb117ef2eead54141a490018ab8f53850
ummmmm <ummmmm@users.noreply.github.com>
2015-08-19 16:47:04 -0700

    Converted the "Module" to use the new Screen class

---
+++ html/includes/runtime/admin/screens/Games.php
@@ -0,0 +1,102 @@
+<?php
+
+class Screen_Games implements iScreen
+{
+	public function __construct( Database &$db, Authentication &$auth, Screen &$screen )
+	{
+		$this->_db		= $db;
+		$this->_auth	= $auth;
+		$this->_screen	= $screen;
+	}
+
+	public function requirements()
+	{
+		return array( "admin" => true );
+	}
+
+	public function jquery()
+	{
+		print "$.fn.load_games();";
+
+		return true;
+	}
+
+	public function content()
+	{
+		$db_teams = new Teams( $this->_db );
+		$count = $db_teams->List_Load( $teams );
+		
+		if ( $count === false )
+		{
+			return $this->_screen->setDBError();
+		}
+		
+		print '<h1>Edit Weeks</h1>';
+		print '<div id="weeks_loading">Loading...</div>';
+		print '<div id="games_addedit">';
+		print '<div id="games_addedit_dialog">Edit Game</div>';
+		print '<a href="javascript:;" id="games_addedit_switch">Switch View</a>';
+		print '<div id="games">';
+		print '<table>';
+		print '<tr>';
+		print '<td>';
+		print '<select id="games_addedit_away" name="away">';
+			Draw::Teams( $teams );
+		print '</select>';
+		print ' <b>vs.</b> ';	
+		print '<select id="games_addedit_home" name="home">';
+			Draw::Teams( $teams );
+		print '</select>';
+		print '</td>';
+		print '<tr>';
+		print '<td>';
+		print '<select id="games_addedit_month" name="month">';
+			Draw::Months();
+		print '</select>';
+		print '<select id="games_addedit_day" name="day">';
+			Draw::Days();
+		print '</select>';
+		print '<select id="games_addedit_year" name="year">';
+			Draw::Years();
+		print '</select>';
+		print '<select id="games_addedit_hour" name="hour">';
+			Draw::Hours();
+		print '</select>';
+		print '<select id="games_addedit_minute" name="minute">';
+			Draw::Minutes();
+		print '</select>';
+		print '</td>';
+		print '</tr>';
+		print '<tr>';
+		print '<td>';
+		print '<select id="games_addedit_week" name="week">';
+			Draw::Weeks();
+		print '</select>';
+		print '</td>';
+		print '</tr>';
+		print '</table>';
+		print '</div>';
+		print '<div id="scores">
+				<table>
+				<tr>
+					<td><b></b>:</td>
+					<td><input type="text" maxlength="2" size="2" id="games_addedit_away_score" name="awayScore" /></td>
+				</tr>
+				<tr>
+					<td><b></b>:</td>
+					<td><input type="text" maxlength="2" size="2" id="games_addedit_home_score" name="homeScore" /></td>
+				</tr>
+				</table>
+				</div>';
+		print Draw::Hidden( 'scored', 'false' );
+		print '<div class="buttons_left">
+			<input type="button" id="games_addedit_cancel" value="Cancel" />
+		</div>
+		<div class="buttons_right">
+			<input type="button" id="games_addedit_update" value="Update Game" />
+		</div>';
+		print '</div>';
+		
+		return true;
+	}
+}
