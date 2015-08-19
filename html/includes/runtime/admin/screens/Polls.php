c552e86c13741d40a881d700867cff2aeb3d1324
ummmmm <ummmmm@users.noreply.github.com>
2015-08-19 16:31:21 -0700

    Converted the "Module" to use the new screen class

---
+++ html/includes/runtime/admin/screens/Polls.php
@@ -0,0 +1,57 @@
+<?php
+
+class Screen_Polls implements iScreen
+{
+	public function __construct( Database &$db, Authentication &$auth, Screen &$screen )
+	{
+		$this->_db		= $db;
+		$this->_auth	= $auth;
+		$this->_screen	= $screen;
+	}
+
+	public function requirements()
+	{
+		return array( "admin" => true );
+	}
+
+	public function jquery()
+	{
+		print "$.fn.load_polls();";
+
+		return true;
+	}
+
+	public function content()
+	{
+		print '<a href="javascript:;" onclick="$.fn.add_poll();">Add Poll</a>';
+		print '<div id="polls_loading">Loading...</div>';
+		print <<<EOT
+				<div id="polls_addedit">
+					<div id="polls_addedit_dialog">Add/Edit Provider</div>
+					<a href="javascript:;" onclick="$.fn.add_poll_answer( null );">Add Answer</a>
+					<table>
+						<tr>
+							<td><b>Question:</b></td>
+							<td><input type="text" name="question" id="polls_addedit_question" /></td>
+						</tr>
+						<tr>
+							<td valign="top"><b>Status:</b></td>
+							<td><input type="radio" name="active" value="1" id="polls_addedit_active" /> Active<br />
+								<input type="radio" name="active" value="0" id="polls_addedit_inactive" /> Inactive
+							</td>
+						</tr>
+					</table>
+					
+					<div class="buttons_left">
+						<input type="button" id="polls_addedit_cancel" value="Cancel" />
+						<input type="button" id="polls_addedit_delete" value="Delete" />
+					</div>
+					<div class="buttons_right">
+						<input type="button" id="polls_addedit_update" value="Update Poll" />
+					</div>
+				</div>
+EOT;
+
+	return true;
+	}
+}
