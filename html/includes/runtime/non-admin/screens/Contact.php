695d4711280d6b205840f7ab38cec2469136bc06
ummmmm <ummmmm@users.noreply.github.com>
2015-08-19 13:16:26 -0700

    Converted the Contact module into the new Screen class

---
+++ html/includes/runtime/non-admin/screens/Contact.php
@@ -0,0 +1,112 @@
+<?php
+
+require_once( "includes/classes/Mail.php" );
+
+class Screen_Contact implements iScreen
+{
+	public function __construct( Database &$db, Authentication &$auth, Screen &$screen )
+	{
+		$this->_db		= $db;
+		$this->_auth	= $auth;
+		$this->_screen	= $screen;
+	}
+
+	public function validate()
+	{
+		$name 		= Functions::Post( "name" );
+		$email		= Functions::Post( "email" );
+		$subject	= Functions::Post( "subject" );
+		$message	= Functions::Post( "message" );
+		$errors		= array();
+
+		if ( $name == "" )
+		{
+			array_push( $errors, "Please enter your name." );
+		}
+
+		if ( $email == "" )
+		{
+			array_push( $errors, "Please enter an email address." );
+		}
+
+		if ( $message == "" )
+		{
+			array_push( $errors, "Please enter a message." );
+		}
+
+		if ( $subject == "" )
+		{
+			$subject = "No Subject";
+		}
+
+		if ( !empty( $errors ) )
+		{
+			return $this->_screen->setValidationErrors( $errors );
+		}
+
+		return $this->_screen->setValidationData( array( "name" => $name, "email" => $email, "subject" => $subject, "message" => $message ) );
+	}
+
+	public function update( $data )
+	{
+		$db_settings = new Settings( $this->_db );
+
+		if ( !$db_settings->Load( $settings ) )
+		{
+			return $this->setDBError();
+		}
+
+		$message	= sprintf( "Name: %s<br />Email: %s<br /><br />Message: %s", $data[ 'name' ], $data[ 'email' ], $data[ 'message' ] );
+		$mail 		= new Mail( $settings[ 'domain_email' ], $data[ 'subject' ], $message );
+
+		$mail->replyto( $data[ 'email' ] );
+
+		if ( $mail->send() === false )
+		{
+			return $this->_screen->setError( array( "#Error#", "Failed to send email.  Please try again later." ) );
+		}
+
+		return $this->_screen->setUpdateMessage( "Your message has been sent and you should receive a response within the next 24 hours." );
+	}
+
+	public function requirements()
+	{
+		return array();
+	}
+
+	public function content()
+	{
+		$name 		= Functions::Post( 'name' );
+		$email		= Functions::Post( 'email' );
+		$subject	= Functions::Post( 'subject' );
+		$message	= Functions::Post( 'message' );
+
+		if ( $this->_auth->getUserID() )
+		{
+			$name	= ( !$name ) ? $this->_auth->getUser()[ 'name' ] : $name;
+			$email	= ( !$email ) ? $this->_auth->getUser()[ 'email' ] : $email;
+		}
+?>
+		<form name="contact" action="?screen=contact" method="post" id="contact">
+		<fieldset>
+			<legend>Contact Form</legend>
+			<label for="name">Your Name</label>
+			<input type="text" name="name" id="name" value="<?php print htmlentities( $name ); ?>" />
+			<br />
+			<label for="email">Your Email</label>
+			<input type="text" name="email" id="email" value="<?php print htmlentities( $email ); ?>" />
+			<br />
+			<label for="subject">Subject</label>
+			<input type="text" name="subject" id="subject" value="<?php print htmlentities( $subject ); ?>" />
+			<br />
+			<label for="message">Message</label>
+			<textarea name="message" cols="50" rows="10" id="message"><?php print htmlentities( $message ); ?></textarea>
+			<br />
+			<input type="hidden" name="update" value="1" />
+			<input type="submit" name="contact" id="contact" value="Send Me Now!" />
+		</fieldset>
+	</form>
+<?php
+		return true;
+	}
+}
