eec77856fce9aeca3353a4b381186991386a77b8
ummmmm <ummmmm@users.noreply.github.com>
2015-08-19 13:22:42 -0700

    Converted the "Module" into the new Screen class

---
+++ html/includes/runtime/non-admin/screens/Schedule.php
@@ -0,0 +1,76 @@
+<?php
+
+class Screen_Schedule implements iScreen
+{
+	public function __construct( Database &$db, Authentication &$auth, Screen &$screen )
+	{
+		$this->_db		= $db;
+		$this->_auth	= $auth;
+		$this->_screen	= $screen;
+	}
+
+	public function requirements()
+	{
+		return array();
+	}
+
+	public function content()
+	{
+		$db_games	= new Games( $this->_db );
+		$db_teams	= new Teams( $this->_db );
+		$db_weeks	= new Weeks( $this->_db );
+		$week_id 	= Functions::Get( 'week' );
+
+		if ( !Validation::Week( $week_id ) )
+		{
+			$count = $db_weeks->List_Load( $weeks );
+
+			if ( $count === false )
+			{
+				return false;
+			}
+
+			print '<h1>Weeks</h1>';
+
+			foreach( $weeks as $loaded_week )
+			{
+				print '<p><a href="?screen=schedule&week=' . $loaded_week[ 'id' ] . '">Week ' . $loaded_week[ 'id' ]. '</a></p>';
+			}
+
+			return true;
+		}
+
+		if ( !$db_weeks->Load( $week_id, $loaded_week ) )
+		{
+			return false;
+		}
+
+		$count = $db_games->List_Load_Week( $week_id, $games );
+
+		if ( $count === false )
+		{
+			return false;
+		}
+
+		$count = $db_teams->Byes( $week_id, $teams );
+
+		if ( $count === false )
+		{
+			return false;
+		}
+
+		print '<h1>Week ' . $week_id . '</h1>';
+
+		foreach( $games as $game )
+		{
+			print "<p>{$game[ 'awayTeam' ]} ({$game[ 'awayWins' ]} - {$game[ 'awayLosses' ]}) <b>vs.</b> {$game[ 'homeTeam' ]} ({$game[ 'homeWins' ]} - {$game[ 'homeLosses' ]})</p>";
+		}
+
+		if ( is_null( $teams[ 'bye_teams' ] ) === false )
+		{
+			print '<p><b>Bye Teams:</b> ' . $teams[ 'bye_teams' ] . '</p>';
+		}
+
+		return true;
+	}
+}
