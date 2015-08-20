8246b8d4190452da3c38a338b9f0e7549faad91b
ummmmm <ummmmm@users.noreply.github.com>
2015-08-19 17:56:59 -0700

    Basically re-wrote the Screen class
    The index.php file now uses the Screen class.  It has little knowledge of
    how the backend handles any of its functionality.  It will still need to be
    modified more so it can get rid of the extra calls to the Users and Settings
    class though.

--- html/includes/classes/Screen.php
+++ html/includes/classes/Screen.php
@@ -3,163 +3,176 @@
 require_once( "Database.php" );
 require_once( "Authentication.php" );
 require_once( "functions.php" );
+require_once( "validation.php" );
 
 interface iScreen
 {
+	function requirements();
 	function content();
 }
 
 class Screen
 {
+	const FLAG_REQ_USER 			= 0x1;
+	const FLAG_REQ_ADMIN			= 0x2;
+	const FLAG_REQ_TOKEN			= 0x4;
+
+	const FLAG_ERROR_NONE			= 0x0;
+	const FLAG_ERROR_MISCONFIGURED	= 0x1;
+	const FLAG_ERROR_VALIDATE		= 0x2;
+	const FLAG_ERROR_UPDATE			= 0x3;
+	const FLAG_ERROR_HEAD			= 0x4;
+	const FLAG_ERROR_JQUERY			= 0x5;
+	const FLAG_ERROR_CONTENT		= 0x6;
+
 	private $_db;
 	private $_auth;
 	private $_screen;
 	private $_error;
 	private $_misconfigured;
+	private $_error_level;
 
 	private $_validation_data;
 	private $_head_data;
+	private $_jquery_data;
 	private $_content_data;
+	private $_execute_success;
 
 	public function __construct()
 	{
-		$this->_db 		= new Database();
-		$this->_auth	= new Authentication();
-		$this->_screen	= null;
-		$this->_error	= null;
-
-		$this->_validation_error	= null;
+		$this->_db 					= new Database();
+		$this->_auth				= new Authentication();
+		
+		$this->_screen				= null;
+		$this->_error				= array();
+		$this->_error_level			= 0x0;
+		$this->_execute_success		= false;
+		$this->_validation_errors	= null;
+		
 		$this->_validation_data 	= null;
+		$this->_jquery_data			= null;
 		$this->_head_data			= null;
 		$this->_content_data		= null;
+
+		$this->_update_message		= null;
+		$this->_run_update			= false;
 	}
 
-	public function initialize( $admin, $screen )
+	public function initialize( $admin, $screen, $update )
 	{
-		$this->_misconfigured = !$this->_configure( $admin, $screen );
+		$this->_misconfigured = !$this->_configure( $admin, $screen, $update );
 	}
 
-	public function execute( $update )
+	public function execute()
 	{
 		if ( $this->_misconfigured )
 		{
-			return false;
+			return $this->_setErrorLevel( self::FLAG_ERROR_MISCONFIGURED );
 		}
 
-		if ( $update )
+		if ( $this->_run_update )
 		{
-			if ( method_exists( $this->_screen, "validate" ) )
+			
+			if ( !$this->_screen->validate() )
 			{
-				if ( $this->_screen->validate() )
-				{
-					if ( method_exists( $this->_screen, "update" ) )
-					{
-						if ( !$this->_screen->update( $this->_getValidationData() ) )
-						{
-							return false;
-						}
-					}
-				}
+				$this->_setErrorLevel( self::FLAG_ERROR_VALIDATE );
+			}
+
+			if ( !$this->_validation_errors && !$this->_screen->update( $this->_validation_data ) )
+			{
+				$this->_setErrorLevel( self::FLAG_ERROR_UPDATE );
 			}
 		}
 
 		if ( method_exists( $this->_screen, "head" ) )
 		{
 			ob_start();
-			if ( !$this->_screen->head() )
-			{
-				return false;
-			}
-
-			$this->_setHeadData( ob_get_contents() );
+			if ( !$this->_screen->head() )	$this->_setErrorLevel( self::FLAG_ERROR_HEAD );
+			else							$this->_setHeadData( ob_get_contents() );
 			ob_clean();
 		}
 
-		ob_start();
-		if ( !$this->_screen->content() )
+		if ( method_exists( $this->_screen, "jquery" ) )
+		{
+			ob_start();
+			if ( !$this->_screen->jquery() )	$this->_setErrorLevel( self::FLAG_ERROR_JQUERY );
+			else								$this->_setJQueryData( ob_get_contents() );
+			ob_clean();
+		}
+
+		if ( $this->_error_level != 0x0 )
 		{
 			return false;
 		}
-
+		
+		ob_start();
+		if ( !$this->_screen->content() )
+		{
+			return $this->_setErrorLevel( self::FLAG_ERROR_CONTENT );
+		}
 		$this->_setContentData( ob_get_contents() );
 		ob_clean();
+
+		$this->_execute_success = true;
 
 		return true;
 	}
 
-	public function getValidationErrorsData()
+	public function head()
 	{
-		$errors = $this->_validation_error;
-
-		if ( is_null( $errors ) )
+		if ( $this->_execute_success )
 		{
-			return;
+			return $this->_head_data;
 		}
+	}
 
-		if ( !is_array( $errors ) )
+	public function jquery_head()
+	{
+		if ( $this->_execute_success )
 		{
-			$errors = array( $errors );
+			return $this->_jquery_data;
 		}
+	}
 
-		$count = count( $errors );
-
-		if ( $count > 0 )
+	public function content()
+	{
+		ob_start();
+		switch ( $this->_error_level )
 		{
-			$output 	= '';
-			$message	= '';
-			$title 		= ( $count === 1 ) ? 'Error Has' : $count . ' Errors Have';
-
-			foreach( $errors as $error )
+			case self::FLAG_ERROR_HEAD			:
+			case self::FLAG_ERROR_JQUERY		:
+			case self::FLAG_ERROR_CONTENT		:
+			case self::FLAG_ERROR_VALIDATE		:
+			case self::FLAG_ERROR_UPDATE		:
+			case self::FLAG_ERROR_MISCONFIGURED	:
+			default								:
 			{
-				$message .= "- {$error}<br />";
+				$this->_outputFatalError();
+				break;
 			}
-
-			print '<div class="error">';
-			printf( '<span class="error_text_top">The Following %s Ocurred!</span><br />', htmlentities( $title ) );
-			printf( '<span class="error_text">%s</span>', $message );
-			print '</div>';
+			case self::FLAG_ERROR_NONE			:
+			{
+				$this->_outputUpdateMessage();
+				$this->_outputValidationErrors();
+				$this->_outputContentData();
+				break;
+			}
 		}
+		$data = ob_get_contents();
+		ob_clean();
+
+		return $data;
 	}
 
-	public function setValidationErrors( $errors )
-	{
-		$this->_validation_error = $errors;
-
-		return false;
-	}
-
-	private function _getValidationData()
-	{
-		return $this->_validation_data;
-	}
-
-	private function _setHeadData( $data )
-	{
-		$this->_head_data = $data;
-	}
-
-	private function _setContentData( $data )
-	{
-		$this->_content_data = $data;
-	}
-
-	public function getContentData()
-	{
-		return $this->_content_data;
-	}
-
-	public function getHeadData()
-	{
-		return $this->_head_data;
-	}
-
-	private function _configure( $admin, $screen )
+	private function _configure( $admin, $screen, $run_update )
 	{
 		if ( $admin )	$path = "includes/runtime/admin/screens";
 		else			$path = "includes/runtime/non-admin/screens";
 
-		$class 		= sprintf( "Screen_%s", $screen );
-		$file_path	= sprintf( "%s/%s.php", $path, Functions::Strip_Nulls( $screen ) );
+		$screen			= ( $screen == "" ) ? "default" : $screen;
+		$screen_name 	= $this->_screenName( $screen );
+		$class 			= sprintf( "Screen_%s", $screen_name );
+		$file_path		= sprintf( "%s/%s.php", $path, Functions::Strip_Nulls( $screen_name ) );
 
 		if ( !file_exists( $file_path ) )
 		{
@@ -182,6 +195,33 @@ class Screen
 			return $this->_setError( array( "#Error#", "Screen is missing required interface" ) );
 		}
 
+		$this->_getRequirements( $flags );
+
+		if ( ( $flags & self::FLAG_REQ_USER ) && !$this->_auth->isUser() )
+		{
+			return $this->_setError( array( '#Error#', 'You must be a user to complete this action' ) );
+		}
+
+		if ( ( $flags & self::FLAG_REQ_ADMIN ) && !$this->_auth->isAdmin() )
+		{
+			return $this->_setError( array( '#Error#', 'You must be an administrator to complete this action' ) );
+		}
+
+		if ( ( $flags & self::FLAG_REQ_TOKEN ) && !$this->_auth->isValidToken( $token ) )
+		{
+			return $this->_setError( array( '#Error#', 'You must have a valid token to complete this action' ) );
+		}
+
+		if ( $run_update )
+		{
+			if ( !method_exists( $this->_screen, "validate" ) || !method_exists( $this->_screen, "update" ) )
+			{
+				return $this->_setError( array( "#Error#", "Screen is missing required methods" ) );
+			}
+
+			$this->_run_update = true;
+		}
+
 		return true;
 	}
 
@@ -192,15 +232,183 @@ class Screen
 		return false;
 	}
 
-	public function getError()
+	private function _setErrorLevel( $level )
 	{
-		return $this->_error;
+		$this->_error_level = $level;
+
+		return false;
+	}
+
+	private function _setJQueryData( $data )
+	{
+		$this->_jquery_data = $data;
+	}
+
+	private function _setHeadData( $data )
+	{
+		$this->_head_data = $data;
+	}
+
+	private function _setContentData( $data )
+	{
+		$this->_content_data = $data;
+	}
+
+	private function _outputUpdateMessage()
+	{
+		if ( !$this->_update_message )
+		{
+			return;
+		}
+
+		printf( "<p><b>%s</b></p>", htmlentities( $this->_update_message ) );
+	}
+
+	private function _outputContentData()
+	{
+		print $this->_content_data;
+	}
+
+	private function _outputFatalError()
+	{
+		@list( $code, $message ) = $this->_error;
+
+		$code 		= ( !$code ) ? "UNKNOWN" : $code;
+		$message 	= ( !$message ) ? "An unknown error has occurred." : $message;
+
+		printf( "<h1>An error has occurred</h1>\n" );
+		printf( "<div>Error Code: %s</div>\n", htmlentities( $code ) );
+		printf( "<div>Error Message: %s</div>\n", htmlentities( $message ) );
+	}
+
+	private function _outputValidationErrors()
+	{
+		$errors = $this->_validation_errors;
+
+		if ( !$errors )
+		{
+			return;
+		}
+
+		$count 		= count( $errors );
+		$title 		= ( $count == 1 ) ? "Error Has" : sprintf( "%d Errors Have", $count );
+		$message	= "";
+
+		foreach( $errors as $error )
+		{
+			$message .= sprintf( "- %s<br />\n", htmlentities( $error ) );
+		}
+
+		printf( "<div class=\"error\">\n" );
+		printf( "<span class=\"error_text_top\">The Following %s Ocurred!</span><br />\n", $title );
+		printf( "<span class=\"error_text\">%s</span>\n", $message );
+		printf( "</div>\n" );
+	}
+
+	private function _screenName( $screen )
+	{
+		return implode( "", array_map( function( $string ) { return ucfirst( $string ); }, explode( '_', $screen ) ) );
+	}
+
+	private function _getRequirements( &$flags )
+	{
+		$requirements	= $this->_screen->requirements();
+		$flags			= 0x0;
+		$flags			|= array_key_exists( 'user', 	$requirements ) && $requirements[ 'user' ] 	? self::FLAG_REQ_USER	: 0x0;
+		$flags			|= array_key_exists( 'admin', 	$requirements ) && $requirements[ 'admin' ] ? self::FLAG_REQ_ADMIN	: 0x0;
+		$flags			|= array_key_exists( 'token', 	$requirements ) && $requirements[ 'token' ] ? self::FLAG_REQ_TOKEN	: 0x0;
+	}
+
+	// public functions that the sub-classes can use to set data/errors
+
+	public function setError( $error )
+	{
+		$this->_error = $error;
+
+		return false;
+	}
+
+	public function setDBError()
+	{
+		return $this->_setError( $this->_db->Get_Error() );
 	}
 
 	public function setValidationData( $data )
 	{
 		$this->_validation_data = $data;
+
+		return true;
 	}
 
+	public function setUpdateMessage( $message )
+	{
+		$this->_update_message = $message;
+
+		return true;
+	}
+
+	public function setValidationErrors( $errors )
+	{
+		$this->_validation_errors = $errors;
+
+		return true;
+	}
+
+	// stupid navigation stuff
+
+	public function topNavigation()
+	{
+		$db_weeks 	= new Weeks( $this->_db );
+		$user		= $this->_auth->getUser();
+
+		if ( !$this->_auth->getUserID() )
+		{
+			printf( "<span>Welcome, Guest. Please login to start making your picks for week %d.</span>\n", $db_weeks->Current() );
+		}
+		else
+		{
+			printf( "<span>Welcome, %s!  You have %d wins and %d losses and currently in %s place.</span>\n", htmlentities( $user[ 'name' ] ), $user[ 'wins' ], $user[ 'losses' ], Functions::Place( $user[ 'current_place' ] ) );
+		}
+	}
+
+	public function sideNavigation()
+	{
+		$db_weeks	= new Weeks( $this->_db );
+		$weekid 	= $db_weeks->Previous();
+		$admin 		= ( $this->_auth->isAdmin() ) ? '<li><a href="?view=admin" title="Admin Control Panel">Admin Control Panel</a></li>' : '';
+
+		if ( $this->_auth->getUserID() )
+		{
+			print <<<EOT
+<h1>User Links</h1>
+<ul>
+	<li><a href="" title="Home">Home Page</a></li>
+	<li><a href="?screen=control_panel" title="User Control Panel">User Control Panel</a></li>
+	{$admin}
+	<li><a href="?screen=make_picks" title="Make Picks">Make Picks</a></li>
+	<li><a href="?screen=view_picks&week={$weekid}" title="User Picks">View Other's Picks</a></li>
+	<li><a href="?screen=weekly_records" title="Weekly User Records">View Weekly Records</a></li>
+	<li><a href="?screen=leaderboard" title="Leader Board">View Leader Board</a></li>
+	<li><a href="?screen=logout" title="Logout" id="logout">Logout</a></li>
+</ul>
+EOT;
+		}
+
+		print "<h1>Quick Links</h1>\n";
+		print "<ul>";
+
+		if ( !$this->_auth->getUserID() )
+		{
+			print "<li><a href=\"?screen=register\" title=\"Register\">Register</a></li>";
+			print "<li><a href=\"?screen=login\" title=\"Login\">Login</a></li>";
+		}
+
+		print <<<EOT
+<li><a href="?screen=schedule" title="View Schedule">Schedule</a></li>
+<li><a href="?screen=contact" title="Contact Us">Contact Us</a></li>
+<li><a href="?screen=online" title="Online Users">Online Users</a></li>
+</ul>
+EOT;
+	}
 
 }
--- html/index.php
+++ html/index.php
@@ -2,108 +2,80 @@
 session_start();
 ob_start();
 
-require_once( 'includes/classes/functions.php' );
-require_once( 'includes/classes/Database.php' );
-require_once( 'includes/classes/validation.php' );
+header( "Content-Type: text/html; charset=utf-8" );
+
+require_once( "includes/classes/functions.php" );
+require_once( "includes/classes/Database.php" );
 require_once( "includes/classes/Screen.php" );
 
-$screen 			= new Screen();
-$db					= new Database();
-$users				= new Users( $db );
-$settings			= new Settings( $db );
-
+$screen 	= new Screen();
+$db			= new Database();
+$users		= new Users( $db );
+$settings	= new Settings( $db );
 $admin 		= Functions::Get( "view" ) == "admin" ? true : false;
-$screen1	= Functions::Get( "screen" ) == "" ? "index" : Functions::Get( "screen" );
-$update		= Functions::Post( "update" ) == "update" ? true : false;
 
-$screen->initialize( $admin, ucfirst( $screen1 ) );
-$success			= $screen->execute( $update );
+$screen->initialize( $admin, Functions::Get( "screen" ), Functions::Post_Int( "update" ) );
+$screen->execute();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta charset="utf-8" />
-<title><?php printf( '%s', htmlentities( $settings->site_title ) ); ?></title>
-<base href="<?php print $settings->domain_url; ?>" />
-<link rel="icon" type="image/x-icon" href="static/favicon.ico" />
-<link rel="stylesheet" type="text/css" href="static/css/styles.css" media="screen" />
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js" type="text/javascript"></script>
-<script src="static/javascript/jqueryui.js" type="text/javascript"></script>
-<script src="static/javascript/javascript.js" type="text/javascript"></script>
-<script type="text/javascript">
-	var json_url = <?php print json_encode( sprintf( "json.php?token=%s", urlencode( $users->token ) ) ); ?>;
-	var token = <?php print json_encode( $users->token ); ?>;
-</script>
-<script type="text/javascript">$( document ).ready( function() { $.fn.load_poll(); } );</script>
-<?php
-	if ( $admin )
-	{
-		print '<script type="text/javascript src="static/javascript/admin.js"></script>';
-	}
+	<title><?php printf( '%s', htmlentities( $settings->site_title ) ); ?></title>
+	<base href="<?php print $settings->domain_url; ?>" />
+	<link rel="icon" type="image/x-icon" href="static/favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="static/css/styles.css" media="screen" />
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js" type="text/javascript"></script>
+	<script src="static/javascript/jqueryui.js" type="text/javascript"></script>
+	<script src="static/javascript/javascript.js" type="text/javascript"></script>
+	<script type="text/javascript">
+		var json_url = <?php print json_encode( sprintf( "json.php?token=%s", urlencode( $users->token ) ) ); ?>;
+		var token = <?php print json_encode( $users->token ); ?>;
+	</script>
+	<?php
+		if ( $admin )
+		{
+			print '<script type="text/javascript" src="static/javascript/admin.js"></script>';
+		}
 
-	if ( $success )
-	{
-		print $screen->getHeadData();
-	}
-?>
-<script>
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		print $screen->head();
+	?>
+	<script type="text/javascript">$( document ).ready( function() { $.fn.load_poll(); } );</script>
+	<script type="text/javascript">$( document ).ready( function() { <?php print $screen->jquery_head(); ?> } );</script>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	ga('create', 'UA-2438906-1', 'auto');
-	ga('send', 'pageview');
-</script>
+		ga('create', 'UA-2438906-1', 'auto');
+		ga('send', 'pageview');
+	</script>
 </head>
 <body>
-<div class="container">
-  <div class="header">
-    <div class="title">
-      <h1><a href="" title="<?php printf( "%s", htmlentities( $settings->site_title ) ); ?>"><?php printf( "%s", htmlentities( $settings->site_title ) ); ?></a></h1>
-    </div>
-  </div>
-  <div class="navigation">
-    <?php Functions::TopNavigation( $db, $users ); ?>
-  </div>
-  <div class="main">
-    <div class="content">
-	<?php
-		if ( $success )
-		{
-			print $screen->getValidationErrorsData();
-			print $screen->getContentData();
-		}
-		else
-		{
-			Functions::OutputError( $screen->getError() );
-		}
-	?>
-    </div>
-    <div class="sidenav">
-      <?php Functions::UserNavigation( $db, $users ); ?>
-      <h1>Quick Links</h1>
-      <ul>
-      <?php
-      	if ( $users->logged_in === false )
-      	{
-      		print '<li><a href="?screen=register" title="Register">Register</a></li>';
-      		print '<li><a href="?screen=login" title="Login">Login</a></li>';
-      	}
-      ?>
-        <li><a href="?screen=schedule" title="View Schedule">Schedule</a></li>
-        <li><a href="?screen=contact" title="Contact Us">Contact Us</a></li>
-        <li><a href="?screen=online" title="Online Users">Online Users</a></li>
-      </ul>
-      <h1>Poll</h1>
-		<div id="loading_polls_nav"></div>
-    </div>
-    <br clear="all" />
-  </div>
-  <div class="footer">
-	  &copy; 2007-2015 <a href="">NFLPick-Em.com</a>. Template design by <a href="http://templates.arcsin.se" target="_blank" title="Designed By Arcsin">Arcsin</a>.
-  </div>
-</div>
+	<div class="container">
+		<div class="header">
+			<div class="title">
+				<h1><a href="" title="<?php printf( "%s", htmlentities( $settings->site_title ) ); ?>"><?php printf( "%s", htmlentities( $settings->site_title ) ); ?></a></h1>
+			</div>
+		</div>
+		<div class="navigation">
+			<?php print $screen->topNavigation(); ?>
+		</div>
+		<div class="main">
+			<div class="content">
+				<?php print $screen->content(); ?>
+			</div>
+			<div class="sidenav">
+				<?php print $screen->sideNavigation(); ?>
+				<h1>Poll</h1>
+				<div id="loading_polls_nav"></div>
+			</div>
+			<br clear="all" />
+		</div>
+		<div class="footer">
+			&copy; 2007-2015 <a href="">NFLPick-Em.com</a>. Template design by <a href="http://templates.arcsin.se" target="_blank" title="Designed By Arcsin">Arcsin</a>.
+		</div>
+	</div>
 </body>
 </html>
 <?php
