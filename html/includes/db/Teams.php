1f5092a0a1c372934fc0960774525a5650053aa0
ummmmm <ummmmm@users.noreply.github.com>
2017-08-25 12:32:16 -0700

    Fixed bug #16.  Changed RAM to LAR

--- html/includes/db/Teams.php
+++ html/includes/db/Teams.php
@@ -168,7 +168,7 @@ class Teams
 
 		array_push( $teams, array( 'team' => 'San Francisco 49ers', 	'conf' => 'NFC West', 	'stadium' => 'Levi\'s Stadium', 					'abbr' => 'SF',		'o_abbr' => 'SFO' ) );
 		array_push( $teams, array( 'team' => 'Seattle Seahawks', 		'conf' => 'NFC West', 	'stadium' => 'CenturyLink Field', 					'abbr' => 'SEA',	'o_abbr' => '' ) );
-		array_push( $teams, array( 'team' => 'Los Angeles Rams', 		'conf' => 'NFC West', 	'stadium' => 'Los Angeles Memorial Coliseum',		'abbr' => 'LA',		'o_abbr' => 'RAM' ) );
+		array_push( $teams, array( 'team' => 'Los Angeles Rams', 		'conf' => 'NFC West', 	'stadium' => 'Los Angeles Memorial Coliseum',		'abbr' => 'LAR',	'o_abbr' => 'LAR' ) );
 
 		return $teams;
 	}
