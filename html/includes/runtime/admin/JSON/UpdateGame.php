bd32b0037d0b749dac66b3bc504582926b935a79
ummmmm <carve006@csusm.edu>
2022-08-20 20:01:50 -0700

    Removed old, awful, and unused functions related to calculating the worst record for a week

--- html/includes/runtime/admin/JSON/UpdateGame.php
+++ html/includes/runtime/admin/JSON/UpdateGame.php
@@ -7,18 +7,18 @@ class JSON_UpdateGame extends JSONAdminAction
 		$db_games	= new Games( $this->_db );
 		$game_id	= Functions::Post_Int( 'game_id' );
 		$scored 	= Functions::Post_Boolean( 'scored' );
-		
+
 		if ( !$db_games->Load( $game_id, $loaded_game ) )
 		{
 			return $this->setError( array( 'NFL-GAMES_UPDATE-6', 'Failed to load game' ) );
 		}
-		
+
 		if ( $scored === true )
 		{
 			return $this->_Update_Scores( $this->_db, $user, $loaded_game );
 		}
-		
-		return $this->_Update_Games( $this->_db, $user, $loaded_game );	
+
+		return $this->_Update_Games( $this->_db, $user, $loaded_game );
 	}
 
 	private function _Update_Scores( &$db, &$user, &$game )
@@ -27,7 +27,7 @@ class JSON_UpdateGame extends JSONAdminAction
 		$db_teams	= new Teams( $db );
 		$awayScore	= Functions::Post_Int( 'awayScore' );
 		$homeScore	= Functions::Post_Int( 'homeScore' );
-		
+
 		if ( $awayScore < 0 || $homeScore < 0 )
 		{
 			return $this->setError( array( 'NFL-GAMES_UPDATE-0', 'Invalid game score' ) );
@@ -49,22 +49,22 @@ class JSON_UpdateGame extends JSONAdminAction
 		$game[ 'homeScore' ]	= $homeScore;
 		$game[ 'awayScore' ] 	= $awayScore;
 		$game[ 'final' ]		= 1;
-		
+
 		if ( !$db_games->Update( $game ) )
 		{
 			return $this->setDBError();
 		}
-		
+
 		if ( !$db_teams->Recalculate_Records() )
 		{
 			return $this->setDBError();
 		}
-		
+
 		if ( !$this->_Users_Update_Record() )
 		{
 			return $this->setDBError();
 		}
-		
+
 		if ( !$db_games->Load( $game[ 'id' ], $game ) )
 		{
 			return $this->setDBError();
@@ -86,62 +86,62 @@ class JSON_UpdateGame extends JSONAdminAction
 		$year		= Functions::Post_Int( 'year' );
 		$hour		= Functions::Post_Int( 'hour' );
 		$minute		= Functions::Post_Int( 'minute' );
-		
+
 		$mktime = mktime( $hour, $minute, 0, $month, $day, $year );
-		
+
 		if ( !checkdate( $month, $day, $year ) || $mktime === false || $hour > 23 || $minute > 59 )
 		{
 			return $this->setError( array( 'NFL-GAMES_UPDATE-1', 'Invalid game date' ) );
 		}
-		
+
 		$team_count = $db_teams->Load( $away_id, $loaded_away );
-		
+
 		if ( $team_count === false )
 		{
 			return $this->setDBError();
 		}
-		
+
 		if ( $team_count === 0 )
 		{
 			return $this->setError( array( 'NFL-GAMES_UPDATE-2', 'Failed to load the away team' ) );
 		}
-		
+
 		$team_count = $db_teams->Load( $home_id, $loaded_home );
-		
+
 		if ( $team_count === false )
 		{
 			return $this->setDBError();
 		}
-		
+
 		if ( $team_count === 0 )
 		{
 			return $this->setError( array( 'NFL-GAMES_UPDATE-3', 'Failed to load the home team' ) );
 		}
-		
+
 		$week_count = $db_weeks->Load( $week_id, $loaded_week );
-		
+
 		if ( $week_count === false )
 		{
 			return $this->setDBError();
 		}
-		
+
 		if ( $week_count === 0 )
 		{
-			return $this->setError( array( 'NFL-GAMES_UPDATE-4', 'Failed to load week' ) ); 
+			return $this->setError( array( 'NFL-GAMES_UPDATE-4', 'Failed to load week' ) );
 		}
-		
+
 		$date = new DateTime( date( 'Y-m-d H:i:s', $mktime ) );
-		
+
 		$game[ 'week' ] = $week_id;
 		$game[ 'away' ] = $away_id;
 		$game[ 'home' ] = $home_id;
 		$game[ 'date' ] = $date->getTimestamp();
-		
+
 		if ( !$db_games->Update( $game ) )
 		{
 			return $this->setDBError();
 		}
-		
+
 		if ( !$this->_Picks_Update_Week( $db, $game[ 'id' ], $week_id ) )
 		{
 			return $this->setDBError();
@@ -151,7 +151,7 @@ class JSON_UpdateGame extends JSONAdminAction
 		{
 			return $this->setDBError();
 		}
-		
+
 		return $this->setData( $game );
 	}
 
@@ -178,55 +178,5 @@ class JSON_UpdateGame extends JSONAdminAction
 		}
 
 		return true;
-	}
-
-	private function _MissedPicksList_Load( &$db, &$users )
-	{
-		return $db->select( 'SELECT u.id AS user_id, w.id AS week_id FROM users u, weeks w WHERE u.id NOT IN ( SELECT p.user_id FROM picks p WHERE p.week = w.id ) AND w.locked = 1', $users );
-	}
-
-	private function _Missed_Week_Record_Update( &$db, $week_id )
-	{
-		$count = $db->single( 'SELECT IF( ( SELECT COUNT( id ) AS played_games FROM games WHERE winner = 0 AND week = ? ) = 0, 1, 0 ) AS all_played', $played_games, $week_id );
-		
-		if ( $count === false )
-		{
-			return false;
-		}
-		
-		if ( $played_games[ 'all_played' ] === 0 )
-		{
-			return true;
-		}
-		
-		$count = $this->_MissedPicksList_Load( $db, $loaded_users );
-		
-		if ( $count === false )
-		{
-			return false;
-		}
-		
-		foreach( $loaded_users as $loaded_user )
-		{
-			$week_id = $loaded_user[ 'week_id' ];
-			$user_id = $loaded_user[ 'user_id' ];
-			$count	 = Functions::Worst_Record_Calculated( $db, $week_id, $record );
-			
-			if ( $count === false )
-			{
-				return false;
-			}
-			
-			$total_games 	= $record[ 'total' ];
-			$wins			= $record[ 'wins' ];
-			$losses			= $record[ 'losses' ];
-			
-			if ( !$db->query( 'UPDATE users SET wins = ( wins + ? ), losses = ( ( losses - ? ) + ? ) WHERE id = ?', $wins, $total_games, $losses, $user_id ) )
-			{
-				return false;
-			}
-			
-			return true;
-		}
 	}
 }
