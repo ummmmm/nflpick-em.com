ec241cb1db16d20b786f9b9556e0a8f2c7b9759b
ummmmm <carve006@csusm.edu>
2022-08-26 20:35:26 -0700

    Added additional check verifying 1 or more picks are selected before sending

--- html/includes/runtime/non-admin/JSON/EmailPicks.php
+++ html/includes/runtime/non-admin/JSON/EmailPicks.php
@@ -23,16 +23,16 @@ class JSON_EmailPicks extends JSONUserAction
 			return $this->setDBError();
 		}
 
+		if ( count( $picks ) === 0 )
+		{
+			return $this->setError( array( '#Error#', 'No picks have been selected' ) );
+		}
+
 		$sent = array( 'userid' => $this->_auth->getUserID(), 'week' => $week, 'date' => Functions::Timestamp(), 'picks' => array() );
 		$mail = new Mail( $this->_auth->getUser()[ 'email' ], sprintf( "Week %d Picks", $week ) );
 
 		foreach( $picks as $pick )
 		{
-			if ( $pick[ 'winner_pick' ] === 0 )
-			{
-				continue;
-			}
-
 			array_push( $sent[ 'picks' ], array( 'winner' => $pick[ 'winner' ], 'loser' => $pick[ 'loser' ] ) );
 		}
 
@@ -55,11 +55,11 @@ class JSON_EmailPicks extends JSONUserAction
 
 	private function _confirmationText( &$week, &$picks )
 	{
-		$output = sprintf( 'Here are your picks for week %d Please save them for your records.<br /><br />', $week );
+		$output = sprintf( 'Here are your picks for week %d. Please save them for your records.<br /><br />', $week );
 
 		foreach( $picks as $pick )
 		{
-			$output .= sprintf( 'You have picked the <b>%s</b> to beat the <b>%s</b><br />', $pick[ 'winner' ], $pick[ 'loser' ] );
+			$output .= sprintf( 'You have picked the <b>%s</b> to beat the <b>%s</b><br />', htmlentities( $pick[ 'winner' ] ), htmlentities( $pick[ 'loser' ] ) );
 		}
 
 		return $output;
