602dccca8ee1b2f8bf5b73de9f268e6162f6cd47
David Carver <carve006@csusm.edu>
2024-12-31 14:01:33 -0800

    67: The poll add/edit screen now shows player votes

--- html/includes/runtime/admin/JSON/LoadPolls.php
+++ html/includes/runtime/admin/JSON/LoadPolls.php
@@ -5,7 +5,6 @@ class JSON_LoadPolls extends JSONAdmin
 	public function execute()
 	{
 		$db_poll_answers	= new Poll_Answers( $this->_db );
-		$db_poll_votes		= new Poll_Votes( $this->_db );
 		$db_polls			= new Polls( $this->_db );
 		$count 				= $db_polls->List_Load( $polls );
 		
@@ -23,18 +22,24 @@ class JSON_LoadPolls extends JSONAdmin
 				return $this->setDBError();
 			}
 			
-			$votes_count = $db_poll_votes->Total_Poll( $poll[ 'id' ] );
+			$count = $this->_load_poll_votes( $poll[ 'id' ], $votes );
 			
-			if ( $votes_count === false )
+			if ( $count === false )
 			{
 				return $this->setDBError();
 			}
 			
 			$poll[ 'date' ] 		= Functions::FormatDate( $poll[ 'date' ] );
 			$poll[ 'answers' ] 		= $answers;
-			$poll[ 'total_votes' ]	= $votes_count;
+			$poll[ 'total_votes' ]	= $count;
+			$poll[ 'votes' ]		= $votes;
 		}
 		
 		return $this->setData( $polls );
+	}
+
+	private function _load_poll_votes( $poll_id, &$votes )
+	{
+		return $this->_db->select( 'SELECT pa.answer, CONCAT( u.fname, \' \', u.lname ) AS name FROM poll_votes pv, poll_answers pa, users u WHERE pv.poll_id = ? AND pv.answer_id = pa.id AND pv.user_id = u.id ORDER BY pv.answer_id, name', $votes, $poll_id );
 	}
 }
