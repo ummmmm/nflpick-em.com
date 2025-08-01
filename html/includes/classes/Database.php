4c126c3add1b189ac46fd4c2532776851ca6cd07
David Carver <carve006@csusm.edu>
2025-07-31 20:49:25 -0700

    77: DatabaseManager now uses the new PHP 8.2+ execute_query function
    The execute_query function replaces the need for the _Run_Statement method
    as it will prepare, bind, execute, and return the results.  This greatly
    simplifies the code base.

--- html/includes/classes/Database.php
+++ html/includes/classes/Database.php
@@ -139,14 +139,13 @@ class DatabaseManager
 
 class DatabaseConnection
 {
-	private $_connected;
 	private $_mysqli;
-	private $_error;
 
 	public function __construct()
 	{
-		$this->_error		= array();
-		$this->_connected	= false;
+		mysqli_report( MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT ); // Force all errors to throw exceptions
+
+		$this->_mysqli = null;
 	}
 
 	public function __destruct()
@@ -156,260 +155,51 @@ class DatabaseConnection
 
 	public function connect( $host, $user, $password, $schema )
 	{
-		try
-		{
-			$this->_mysqli = new mysqli( $host, $user, $password, $schema );
-		}
-		catch ( Exception $e )
-		{
-			return $this->_setError( 'Failed to connect to the database' );
-		}
-
-		$this->_connected = true;
+		$this->_mysqli = new mysqli( $host, $user, $password, $schema );
 
 		return true;
 	}
 
 	public function disconnect()
 	{
-		if ( $this->_connected )
+		if ( $this->_mysqli )
 		{
-			$this->_connected = false;
 			$this->_mysqli->close();
+			$this->_mysqli = null;
 		}
 	}
 
-	public function query( $query )
+	public function query( $query, ...$params )
 	{
-		$bind_parmas = array_slice( func_get_args(), 1 );
+		$ret = $this->_mysqli->execute_query( $query, $params );
 
-		return $this->_Run_Statement( $query, $bind_parmas );
-	}
-
-	public function select( $query, &$results )
-	{
-		$results_count		= 0;
-		$results 			= array();
-		$bind_parmas		= array_slice( func_get_args(), 2 );
-		$multiple_results	= true;
-
-		if ( !$this->_Run_Statement( $query, $bind_parmas, $multiple_results, $results, $results_count ) )
+		if ( $ret !== true )
 		{
-			return false;
-		}
-
-		return $results_count;
-	}
-
-	public function single( $query, &$result )
-	{
-		$result_count		= 0;
-		$result 			= array();
-		$bind_parmas		= array_slice( func_get_args(), 2 );
-		$multiple_results	= false;
-
-		if ( !$this->_Run_Statement( $query, $bind_parmas, $multiple_results, $result, $result_count ) )
-		{
-			return false;
-		}
-
-		return $result_count;
-	}
-
-	public function insert( $table, &$values )
-	{
-		if ( !is_array( $values ) || count( $values ) === 0 )
-		{
-			return false;
-		}
-
-		$columns	= '';
-		$prepared	= '';
-		$args		= array();
-
-		foreach ( $values as $key => $null )
-		{
-			$columns 	.= $columns == '' ? $key : sprintf( ', %s', $key );
-			$prepared	.= $prepared == '' ? '?' : ', ?';
-		}
-
-		array_push( $args, sprintf( 'INSERT INTO %s ( %s ) VALUES ( %s )', $table, $columns, $prepared ) );
-
-		return call_user_func_array( array( $this, 'query' ), array_values( array_merge( $args, $values ) ) );
-	}
-
-	public function insertID()
-	{
-		return $this->_mysqli->insert_id;
-	}
-
-	private function _Run_Statement( &$query, $params, $multiple_results = false, &$results = null, &$result_count = 0 )
-	{
-		$result_count 	= 0;
-		$bind_count 	= count( $params );
-		$stmt 			= $this->_mysqli->stmt_init();
-
-		try
-		{
-			$stmt->prepare( $query );
-		}
-		catch ( Exception $e )
-		{
-			return $this->_setError( array( 'NFL-DATABASE-0', $this->_mysqli->error ) );
-		}
-
-		if ( $bind_count )
-		{
-			if ( $stmt->param_count != $bind_count )
-			{
-				return $this->_setError( array( '#Error#', sprintf( 'Parameter count mismatch, expected %d parameters, found %d', $bind_count, $stmt->param_count ) ) );
-			}
-
-			$bind_params = array( '' ); // initialize the empty array
-
-			for ( $i = 0; $i < $bind_count; $i++ )
-			{
-				switch( gettype( $params[ $i ] ) )
-				{
-					case 'integer'	:
-						$bind_params[ 0 ] .= 'i';
-						break;
-
-					case 'double'	:
-						$bind_params[ 0 ] .= 'd';
-						break;
-
-					case 'string'	:
-						$bind_params[ 0 ] .= 's';
-						break;
-
-					default			:
-						$bind_params[ 0 ] .= 'b';
-						break;
-				}
-
-				$bind_params[ $i + 1 ] = &$params[ $i ];
-			}
-
-			if ( !call_user_func_array( array( $stmt, 'bind_param' ), $bind_params ) )
-			{
-				return $this->_setError( array( 'NFL-DATABASE-1', $this->_mysqli->error ) );
-			}
-		}
-
-		if ( !$stmt->execute() )
-		{
-			return $this->_setError( array( 'NFL-DATABASE-2', $this->_mysqli->error ) );
-		}
-
-		if ( is_null( $results ) ) // Must be an INSERT, UPDATE, DELETE
-		{
-			if ( !$stmt->close() )
-			{
-				return $this->_setError( array( 'NFL-DATABASE-8', $this->_mysqli->error ) );
-			}
-
-			return true;
-		}
-
-		// Otherwise we have a result set
-
-		if ( !$stmt->store_result() )
-		{
-			$stmt->close();
-
-			return $this->_setError( array( 'NFL-DATABASE-3', $this->_mysqli->error ) );
-		}
-
-		if ( ( $result_count = $stmt->num_rows ) == 0 )
-		{
-			$stmt->free_result();
-
-			if ( !$stmt->close() )
-			{
-				return $this->_setError( array( 'NFL-DATABASE-4', $this->_mysqli->error ) );
-			}
-
-			return true;
-		}
-
-		if ( ( $meta = $stmt->result_metadata() ) === false )
-		{
-			$stmt->free_result();
-			$stmt->close();
-
-			return $this->_setError( array( 'NFL-DATABASE-5', $this->_mysqli->error ) );
-		}
-
-		$fields = array();
-
-		while( ( $field = $meta->fetch_field() ) !== false )
-		{
-			$colunm_name			= $field->name;
-			${$colunm_name}			= null; // create a dynamic PHP variable set to the column name
-			$fields[ $colunm_name ] = &${$colunm_name}; // create a reference to the dynamic variable
-		}
-
-		if ( !call_user_func_array( array( $stmt, 'bind_result' ), array_values( $fields ) ) )
-		{
-			$stmt->free_result();
-			$stmt->close();
-
-			return $this->_setError( array( 'NFL-DATABASE-6', $this->_mysqli->error ) );
-		}
-
-		if ( $multiple_results )
-		{
-			$i = 0;
-
-			while ( $stmt->fetch() )
-			{
-				$results[ $i ] = array();
-
-				foreach ( $fields as $key => $value )
-				{
-					$results[ $i ][ $key ] = $value;
-				}
-
-				$i++;
-			}
-		}
-		else
-		{
-			if ( !$stmt->fetch() )
-			{
-				$stmt->free_result();
-				$stmt->close();
-
-				return $this->_setError( array( 'NFL-DATABASE-7', $this->_mysqli->error ) );
-			}
-
-			foreach( $fields as $key => $value )
-			{
-				$results[ $key ] = $value;
-			}
-		}
-
-		$stmt->free_result();
-
-		if ( !$stmt->close() )
-		{
-			return $this->_setError( array( 'NFL-DATABASE-9', $this->_mysqli->error ) );
+			throw new Exception( 'The "query" method does not support result sets' );
 		}
 
 		return true;
 	}
 
-	private function _setError( $error )
+	public function select( $query, &$results, ...$params )
 	{
-		$this->_error = $error;
+		$ret		= $this->_mysqli->execute_query( $query, $params );
+		$results	= $ret->fetch_all( MYSQLI_ASSOC );
 
-		return false;
+		return $ret->num_rows;
 	}
 
-	public function Get_Error()
+	public function single( $query, &$result, ...$params )
 	{
-		return $this->_error;
+		$ret	= $this->_mysqli->execute_query( $query, $params );
+		$result	= $ret->fetch_assoc();
+
+		return $ret->num_rows;
+	}
+
+	public function insertID()
+	{
+		return $this->_mysqli->insert_id;
 	}
 }
 
