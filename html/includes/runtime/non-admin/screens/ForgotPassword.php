dc0b921409d9c549b7779dd29d52292bb065966c
David Carver <carve006@csusm.edu>
2025-08-12 20:43:41 -0700

    Fixed minor formatting issue

--- html/includes/runtime/non-admin/screens/ForgotPassword.php
+++ html/includes/runtime/non-admin/screens/ForgotPassword.php
@@ -161,15 +161,15 @@ EOT;
 
 	function _generateRandomString( int $length ): string
 	{
-	    $characters			= 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*';
-	    $charactersLength	= strlen( $characters );
-	    $randomString		= '';
-	    
-	    for ( $i = 0; $i < $length; $i++ )
-	    {
-	        $randomString	.= $characters[ random_int( 0, $charactersLength - 1 ) ];
-	    }
-	    
-	    return $randomString;
+		$characters			= 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*';
+		$charactersLength	= strlen( $characters );
+		$randomString		= '';
+
+		for ( $i = 0; $i < $length; $i++ )
+		{
+			$randomString	.= $characters[ random_int( 0, $charactersLength - 1 ) ];
+		}
+
+		return $randomString;
 	}
 }
